dea3674c062b06e43d0f6bffcf4a680922289db3
Shreya <153468813+shreyasiva05@users.noreply.github.com>
2025-11-14 23:42:51 -0500

    Update App.jsx
    Adding Tool bar to the App file

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,39 @@
 import React from 'react'
-import { motion } from 'framer-motion'
 import Hero from './components/Hero'
 import Projects from './components/Projects'
 import Skills from './components/Skills'
 import Footer from './components/Footer'
-import Navbar from './components/Navbar'
+import Navbar from "./components/Navbar";
 
-  
-export default function App(){
-    return (
-      <div className="font-sans">
+export default function App() {
+  return (
+    <div className="font-sans">
+
+      {/* Toolbar */}
+      <Navbar />
+
+      {/* Push all content down */}
+      <div className="pt-20">
         <Hero />
+
         <main className="max-w-6xl mx-auto px-6">
           <section className="mt-24" id="projects">
             <h2 className="text-3xl mb-6">Selected Projects</h2>
             <Projects />
           </section>
-  
+
           <section className="mt-24" id="skills">
-            <h2 className="text-3xl mb-6"></h2>
+            <h2 className="text-3xl mb-6">Skills</h2>
             <Skills />
           </section>
         </main>
-  
+
         <Footer />
       </div>
-    )
+
+    </div>
+  )
 }
-  
 
 
   
